b86ed640fc2522f6696d13493f754ad7ea78d8d5
Ansu <ansujohn09@gmail.com>
2026-06-30 10:23:30 +0530

    Ariflow setup

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -4,3 +4,14 @@ variable "environment" {
   description = "The deployment environment (e.g., dev, staging, prod)"
   type        = string
 }
+variable "vpc_subnet_ids" {
+  description = "List of private subnet IDs for MWAA (Must have NAT Gateway access)"
+  type        = list(string)
+  default     = ["subnet-b110c780", "subnet-b7fe72e8"] 
+}
+
+variable "mwaa_security_group_ids" {
+  description = "List of security group IDs for the MWAA environment"
+  type        = list(string)
+  default     = ["sg-aa8cd19e"] 
+}
